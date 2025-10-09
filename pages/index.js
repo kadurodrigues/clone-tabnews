31bd72aa0fa6385cbedf248bdab7856161eb6ecb
Carlos Rodrigues <carlos.rodrigues.silva@outlook.com>
2025-10-09 15:49:56 -0500

    Return next folder

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-  return <h1>The best way to be prepared for the future is doing your best today.</h1>
+  return <h1>The best preparation for tomorrow is doing your best today.</h1>
 }
 
 export default Home;
